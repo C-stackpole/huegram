9d8fe0883ea9dc4c3aa2fbde5fe777cd62209dfc
C-stackpole <89462863+C-stackpole@users.noreply.github.com>
2023-12-01 09:35:42 -0600

    add padding to PostHue

--- src/components/PostHue.tsx
+++ src/components/PostHue.tsx
@@ -11,7 +11,7 @@ const PostHue = (props:Props) => {
   return (
     <div className='flex p-4'>
 
-        <div className='flex flex-col gap-2 p-4 w-64 rounded-3xl justify-center' style={{backgroundColor: color}}>
+        <div className='flex flex-col gap-2 px-4 py-8 w-64 rounded-3xl justify-center' style={{backgroundColor: color}}>
             <input type="text" name="hue" id="hue" onChange={(event) => setColor(event.target.value)}/>
             <a href="" onClick={() => props.addHue(color)} className='btn bg-white rounded-lg text-center'>Post</a>
         </div>
